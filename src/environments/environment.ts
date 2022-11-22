768c077e9f0150f70aa26ca1876be601bbddc225
Edgaras Gustaitis <93922212+EdgarasGust@users.noreply.github.com>
2022-11-22 18:18:37 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,7 @@ export const environment = {
   production: false,
 
   firebase: {
-    apiKey: 'AIzaSyAb21THq0_8tWgJ0seUw0opzEatQPiGRiA',
-    authDomain: 'grozio-subtilybes.firebaseapp.com',
-    databaseURL:
-      'https://grozio-subtilybes-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'grozio-subtilybes',
-    storageBucket: 'grozio-subtilybes.appspot.com',
-    messagingSenderId: '939000491619',
-    appId: '1:939000491619:web:d2dfb182a424424a139256',
+   
   },
 };
 
